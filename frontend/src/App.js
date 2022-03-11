d02fc27f19cd0e8c550b87ec697ca4117514d6a7
ffdevv <97543560+ffdevv@users.noreply.github.com>
2022-03-11 18:39:21 +0100

    Timeline and api setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
+import 'react-image-gallery/styles/css/image-gallery.css';
 import './App.css';
 
 import Timeline from './components/Timeline';
+import Gallery from './components/Gallery';
+import prodApi, {testApi} from './api';
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <Timeline />
+      <Timeline api={testApi}/>
+      <Gallery images={images} />
     </div>
   );
 }
