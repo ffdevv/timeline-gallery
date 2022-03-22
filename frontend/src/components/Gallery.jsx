66c874fd037ce4fea18632f22c082c12f70466c2
ffdevv <97543560+ffdevv@users.noreply.github.com>
2022-03-22 21:33:00 +0100

    heart button inline must become stateful

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState, useRef, forwardRef, useImperativeHandle } from 'react';
 import ImageGallery from 'react-image-gallery';
 import { Spinner } from 'react-bootstrap';
+import { SuitHeartFill, SuitHeart } from "react-bootstrap-icons";
 import { toArrAndMap } from '../helpers';
 
 
 const Gallery = forwardRef(({ images }, ref) => {
   const [imagesArray, imagesMap] = toArrAndMap(images);
+  const [currentIndex, setCurrentIndex] = useState(null);
+  const [favorites, setFavorites] = useState(new Set());
   const imageGallery = useRef(null);
 
   useImperativeHandle(ref, () => ({
@@ -13,6 +16,11 @@ const Gallery = forwardRef(({ images }, ref) => {
       console.debug("move to", idx);
     }
   }))
+
+  const toggleFavorite = (id) => {
+    if (favorites.has(id)) return favorites.delete(id);
+    favorites.add(id)
+  }
 
   const handleImageClick = (event) => {
     console.debug('clicked on image', event.target, 'at index', imageGallery.current.getCurrentIndex());
@@ -33,6 +41,29 @@ const Gallery = forwardRef(({ images }, ref) => {
 
   const handleScreenChange = (fullScreenElement) => {
     console.debug('changing fullscreen to', !!fullScreenElement);
+  }
+
+  const injectCustomControls = () => {
+    const svgProps = {
+      className: 'image-gallery-svg',
+      style: {
+        width: "28px",
+        height: "28px"
+      }
+    }
+    const mkSvg = () => favorites.has(imageGallery.current?.getCurrentIndex()) ? <SuitHeartFill {...svgProps} /> : <SuitHeart {...svgProps} />
+    const svg = mkSvg();
+    return <button
+      className='image-gallery-icon'
+      onClick={(e) => { toggleFavorite(imageGallery.current?.getCurrentIndex());; }}
+      style={{
+        padding: "20px",
+        left: "0px",
+        top: "0px"
+      }}
+    >
+      {svg}
+    </button>
   }
 
   return (
@@ -60,6 +91,7 @@ const Gallery = forwardRef(({ images }, ref) => {
         // slideInterval={parseInt(this.state.slideInterval)}
         // slideOnThumbnailOver={this.state.slideOnThumbnailOver}
         additionalClass="app-image-gallery"
+        renderCustomControls={injectCustomControls}
       // useWindowKeyDown={this.state.useWindowKeyDown}
       />
     </>
